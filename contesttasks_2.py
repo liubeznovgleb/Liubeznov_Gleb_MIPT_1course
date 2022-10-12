75a6c68368a6a802516c07c66290e9fca069383b
liubeznovgleb <113688749+liubeznovgleb@users.noreply.github.com>
2022-10-12 20:31:58 +0300

    Update contesttasks_2.py

--- contesttasks_2.py
+++ contesttasks_2.py
@@ -39,7 +39,53 @@ def task_6():
         print(a)
         a = list(map(lambda x: x.rstrip(), a))
         print(a)
-task_6()
+
+
+
+def task_7(a):
+    return a[0]
+
+with open("input.txt", "r", encoding='utf-8') as f:
+    b = f.readlines()
+    ar = [i.split() for i in b]
+
+    ars = sorted(ar, key = g)
+    maxb = ars[0][3]
+    i = 0
+    b = maxb
+    sets = set()
+    while b == maxb:
+        sets = sets.union({ars[i][2]}, sets)
+        i += 1
+        if i < len(ars):
+            b = ars[i][3]
+        else:
+            break
+    print(*sets)
+    
+    
+def task_8(a):
+    return a[0]
+with open("input.txt", "r", encoding='utf-8') as f:
+    b = f.readlines()
+    ar = [i.split() for i in b]
+
+    ars = sorted(ar, key = g)
+    for i in ars:
+
+        print(*(i[:2] + i[3:4]))
+        
+
+with open("input.txt", "r", encoding='utf-8') as f:
+    b = f.readlines()
+    a = [i.split() for i in b]
+    ar = [[], [], []]
+    for i in a:
+        ar[int(i[2]) - 9].append(i)
+    sb9 = sum(int(ar[0][i][3]) for i in range(len(ar[0]))) / len(ar[0])
+    sb10 = sum(int(ar[1][i][3]) for i in range(len(ar[1]))) / len(ar[1])
+    sb11 = sum(int(ar[2][i][3]) for i in range(len(ar[2]))) / len(ar[2])
+    print(sb9, sb10, sb11)
 
 
 
