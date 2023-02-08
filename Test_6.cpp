b570b3661afc6903c2dda2357f2b15a5b0cacd0c
Liubeznov_Gleb <g.lubeznov@gmail.com>
2023-02-08 14:03:16 +0300

    A = B^2 + C^2

--- Test_6.cpp
+++ Test_6.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
+    int a, b, c, d;
     a = 169;
     b = c = 1;
+    d = 0;
     while (a >= b * b + c * c)
     {   
         while (a >= b * b + c * c)
@@ -16,13 +17,19 @@ int main()
             {
                 cout << b << endl;
                 cout << c << endl;
-                b = c = a;
+                b = c = a + 1;
+                d = 1;
             }
             
         }
         b ++;
         c = 1;
     }
+    if(d == 0)
+    {
+        cout << "Решений нет" << endl;
+    }
+    
         
 
 }
