b9c0d205d798078d89d2907b6467e22b102f6cb9
Liubeznov_Gleb <g.lubeznov@gmail.com>
2023-02-25 19:42:05 +0300

    Доделано

--- week2/F.cpp
+++ week2/F.cpp
@@ -8,17 +8,17 @@ using namespace std;
 int findLastZero(int (&a)[N]) {
     int t = 0;
     int i = N / 2 - 1;
-    int k = 2;
+    int k = 1;
     while (t != 1) {
         if (a[i] == 0 and a[i + 1] == 1) {
             t = 1;
         }
         if (a[i] == 0 and a[i + 1] == 0) {
-            i = i + N / (2 * k);
+            i = i + (N / (2 * k));
             k *= 2;
         }
         if (a[i] == 1 and a[i + 1] == 1) {
-            i = i - N / (2 * k);
+            i = i - (N / (2 * k));
             k *= 2;
         }
     }
