ebf56dac588ecedfc2a0353ffba29918688e0a34
Liubeznov_Gleb <g.lubeznov@gmail.com>
2023-02-27 21:19:48 +0300

    .

--- Labs/2.2.1/2.1.1(graph2).py
+++ Labs/2.2.1/2.1.1(graph2).py
@@ -3,20 +3,25 @@ import matplotlib.pyplot as plt
 
 L = [1 / 40, 1 / 86, 1 / 173, 1 / 188]
 
-D = [0.63555 / 276.1190, 0.503085 / 373.347,
+D = [0.63555 / 276.1190,0.503085 / 373.347,
      0.307308 / 440.002, 0.385336 / 556.09]
+
+
+D_1 = [2000 * 0.63555 / 276.1190, 2000 * 0.503085 / 373.347,
+     2000 * 0.307308 / 440.002, 2000 * 0.385336 / 556.09]
 
 model = np.polyfit(L, D, 1)
 x = [0, 1 / 35]
-y = [model[1], model[0] / 35 + model[1]]
+y = [2000 * model[1], 2000 * (model[0] / 35 + model[1])]
 
 print(x, y)
 
 plt.figure(figsize=(10, 5))
-plt.scatter(L, D, )
+plt.scatter(L, D_1)
 plt.plot(x, y, color='r')
 plt.xlabel(r'$\frac{1}{P}, \frac{1}{торр}$', fontsize=14)
 plt.ylabel(r'$D, \frac{см^2}{c}$', fontsize=14)
 plt.grid(True)
+plt.savefig('graph2')
 plt.show()
 
