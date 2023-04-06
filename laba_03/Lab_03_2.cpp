ba649fda1adc41e58bf6e5825dbd06a1126f8f06
Liubeznov_Gleb <g.lubeznov@gmail.com>
2023-04-06 12:19:29 +0300

    Hi

--- laba_03/Lab_03_2.cpp
+++ laba_03/Lab_03_2.cpp
@@ -9,7 +9,7 @@ using std::endl;
 
 std::default_random_engine rng((int)time(0));
 
-const int N = 100;
+const int N = 1;
 
 struct Node
 {
@@ -21,9 +21,24 @@ struct Node
 };
 
 int Sluch_4() {
-    int arr[] = {1, 2, 3, 4};
+    int arr[] = {
+        1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,
+        15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,
+        29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,
+        43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,
+        57,  58,  59,  60,  61,  62,  63,  64,  65,  66,  67,  68,  69,  70,
+        71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,
+        85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,  96,  97,  98,
+        99,  100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112,
+        113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126,
+        127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140,
+        141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154,
+        155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168,
+        169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182,
+        183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196,
+        197, 198, 199, 200};
     // std::default_random_engine rng(i + 41 * (int)time(0));
-    std::uniform_int_distribution<unsigned> dstr(0, 3);
+    std::uniform_int_distribution<unsigned> dstr(0, 199);
     int x = arr[dstr(rng)];
     for (unsigned counter = 1; counter != 0; --counter)
         return arr[dstr(rng)];
@@ -35,6 +50,7 @@ Node *random_vector(int x, int y) {
     srand(time(NULL));
     v->x = Sluch_4() % x + 1;
     v->y = Sluch_4() % y + 1;
+    cout << v->x << " " << v->y << endl;
     // v->x = x / 2;
     // v->y = y / 2;
     return v;
@@ -49,7 +65,7 @@ void moving_mass(Node *a[N], int x, int y) {
         if (a[i]->x == 1 or a[i]->x == x or a[i]->y == 1 or a[i]->y == y) {
             a[i]->cond = 0;
         } else if (a[i]->cond == 1) {
-            int m = Sluch_4();
+            int m = Sluch_4() % 4 + 1;
             if (m == 1) {
                 a[i]->x = (a[i]->x - 1);
                 a[i]->x_prev = a[i]->x + 1;
@@ -74,8 +90,8 @@ void moving_mass(Node *a[N], int x, int y) {
             }
             if ((a[i]->x == a[j]->x and
                  (a[i]->y - a[j]->y == 1 or a[i]->y - a[j]->y == -1)) or
-                (a[i]->y == a[j]->y and (a[i]->x - a[j]->x == 1 or
-                 a[i]->x - a[j]->x == -1))) {
+                (a[i]->y == a[j]->y and
+                 (a[i]->x - a[j]->x == 1 or a[i]->x - a[j]->x == -1))) {
                 a[i]->cond = 0;
                 a[j]->cond = 0;
             }
@@ -122,7 +138,7 @@ int main() {
     // cin >> x;
     // cin >> y;
     int count = N;
-    for (int j = 20; j < 21; j++) {
+    for (int j = 200; j < 201; j++) {
         int s = 0;
         // cout << "j = " << j << endl;
         x = j - 1;
